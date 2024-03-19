c5699640f70d03ad37daadee240891106f87f262
Oren Leiman <oren.leiman@redpanda.com>
2024-03-19 14:17:56 -0700

    json: Add json_list<T>::emplace for move constructible T

--- include/seastar/json/json_elements.hh
+++ include/seastar/json/json_elements.hh
@@ -22,17 +22,17 @@
 #pragma once
 
 #ifndef SEASTAR_MODULE
-#include <string>
-#include <vector>
-#include <time.h>
 #include <sstream>
+#include <string>
+#include <time.h>
+#include <vector>
 #endif
 
 #include <seastar/core/do_with.hh>
-#include <seastar/core/loop.hh>
-#include <seastar/json/formatter.hh>
-#include <seastar/core/sstring.hh>
 #include <seastar/core/iostream.hh>
+#include <seastar/core/loop.hh>
+#include <seastar/core/sstring.hh>
+#include <seastar/json/formatter.hh>
 #include <seastar/util/modules.hh>
 
 namespace seastar {
@@ -55,8 +55,12 @@ public:
      * The constructors
      */
     json_base_element() noexcept
-            : _mandatory(false), _set(false) {
-    }
+      : _mandatory(false)
+      , _set(false) {}
+
+    explicit json_base_element(bool set) noexcept
+      : _mandatory(false)
+      , _set(set) {}
 
     virtual ~json_base_element() = default;
 
@@ -66,9 +70,7 @@ public:
      * @return true if this is not a mandatory parameter
      * or if it is and it's value is set
      */
-    virtual bool is_verify() noexcept {
-        return !(_mandatory && !_set);
-    }
+    virtual bool is_verify() noexcept { return !(_mandatory && !_set); }
 
     json_base_element& operator=(const json_base_element& o) noexcept {
         // Names and mandatory are never changed after creation
@@ -98,6 +100,9 @@ public:
 template<class T>
 class json_element : public json_base_element {
 public:
+    explicit json_element(T&& val) noexcept
+      : json_base_element(true)
+      , _value(std::forward<T>(val)) {}
 
     /**
      * the assignment operator also set
@@ -105,11 +110,18 @@ public:
      * @param new_value the new value
      * @return the value itself
      */
-    json_element &operator=(const T& new_value) {
+    json_element& operator=(const T& new_value) {
         _value = new_value;
         _set = true;
         return *this;
     }
+
+    json_element& operator=(T&& new_value) {
+        _value = std::forward<T>(new_value);
+        _set = true;
+        return *this;
+    }
+
     /**
      * the assignment operator also set
      * the set value to true.
@@ -117,7 +129,7 @@ public:
      * @return the value itself
      */
     template<class C>
-    json_element &operator=(const C& new_value) {
+    json_element& operator=(const C& new_value) {
         _value = new_value;
         _set = true;
         return *this;
@@ -126,23 +138,21 @@ public:
      * The brackets operator
      * @return the value
      */
-    const T& operator()() const noexcept {
-        return _value;
-    }
+    const T& operator()() const noexcept { return _value; }
 
     /**
      * The to_string return the value
      * formated as a json value
      * @return the value foramted for json
      */
-    virtual std::string to_string() override
-    {
+    virtual std::string to_string() override {
         return formatter::to_json(_value);
     }
 
     virtual future<> write(output_stream<char>& s) const override {
         return formatter::write(s, _value);
     }
+
 private:
     T _value;
 };
@@ -156,18 +166,43 @@ private:
 template<class T>
 class json_list : public json_base_element {
 public:
+    using const_reference = const T&;
 
     /**
      * Add an element to the list.
      * @param element a new element that will be added to the list
      */
-    void push(const T& element) {
+    void push(const_reference element) { push_back(element); }
+
+    void push_back(const_reference element) {
         _set = true;
         _elements.push_back(element);
     }
 
-    virtual std::string to_string() override
-    {
+    void push_back(T&& element) {
+        _set = true;
+        _elements.push_back(std::forward<T>(element));
+    }
+
+    /**
+     * Move an element onto the list. Enabled iff T is move constructible.
+     * @param element a new element that will be added to the list
+     */
+    template<
+      class U = T,
+      class = std::enable_if_t<
+        std::is_same_v<T, U> && std::is_move_constructible_v<U>>>
+    void emplace_back(T&& element) {
+        emplace_back(std::forward<T>(element));
+    }
+
+    template<class... Args>
+    void emplace_back(Args&&... args) {
+        _set = true;
+        _elements.emplace_back(std::forward<Args>(args)...);
+    }
+
+    virtual std::string to_string() override {
         return formatter::to_json(_elements);
     }
 
@@ -178,7 +213,7 @@ public:
     template<class C>
     json_list& operator=(const C& list) {
         _elements.clear();
-        for  (auto i : list) {
+        for (auto i : list) {
             push(i);
         }
         return *this;
@@ -220,7 +255,6 @@ public:
  * reflection
  */
 struct json_base : public jsonable {
-
     virtual ~json_base() = default;
 
     json_base() = default;
@@ -252,8 +286,8 @@ struct json_base : public jsonable {
      * @param name the element name
      * @param mandatory is this element mandatory.
      */
-    virtual void add(json_base_element* element, std::string name,
-            bool mandatory = false);
+    virtual void
+    add(json_base_element* element, std::string name, bool mandatory = false);
 
     std::vector<json_base_element*> _elements;
 };
@@ -264,19 +298,14 @@ struct json_base : public jsonable {
  * The json_void class will be used to mark that the reply should be empty.
  *
  */
-struct json_void : public jsonable{
-    virtual std::string to_json() const {
-        return "";
-    }
+struct json_void : public jsonable {
+    virtual std::string to_json() const { return ""; }
 
     /*!
      * \brief write to an output stream
      */
-    virtual future<> write(output_stream<char>& s) const {
-        return s.close();
-    }
+    virtual future<> write(output_stream<char>& s) const { return s.close(); }
 };
-
 
 /**
  * The json return type, is a helper class to return a json
@@ -293,15 +322,17 @@ struct json_void : public jsonable{
 struct json_return_type {
     sstring _res;
     std::function<future<>(output_stream<char>&&)> _body_writer;
-    json_return_type(std::function<future<>(output_stream<char>&&)>&& body_writer) : _body_writer(std::move(body_writer)) {
-    }
+    json_return_type(
+      std::function<future<>(output_stream<char>&&)>&& body_writer)
+      : _body_writer(std::move(body_writer)) {}
     template<class T>
     json_return_type(const T& res) {
         _res = formatter::to_json(res);
     }
 
-   json_return_type(json_return_type&& o) noexcept : _res(std::move(o._res)), _body_writer(std::move(o._body_writer)) {
-   }
+    json_return_type(json_return_type&& o) noexcept
+      : _res(std::move(o._res))
+      , _body_writer(std::move(o._body_writer)) {}
     json_return_type& operator=(json_return_type&& o) noexcept {
         if (this != &o) {
             _res = std::move(o._res);
@@ -315,32 +346,50 @@ struct json_return_type {
 };
 
 /*!
- * \brief capture a range and return a serialize function for it as a json array.
+ * \brief capture a range and return a serialize function for it as a json
+ * array.
  *
  * To use it, pass a range and a mapping function.
  * For example, if res is a map:
  *
- * return make_ready_future<json::json_return_type>(stream_range_as_array(res, [](const auto&i) {return i.first}));
+ * return make_ready_future<json::json_return_type>(stream_range_as_array(res,
+ * [](const auto&i) {return i.first}));
  */
 template<typename Container, typename Func>
-SEASTAR_CONCEPT( requires requires (Container c, Func aa, output_stream<char> s) { { formatter::write(s, aa(*c.begin())) } -> std::same_as<future<>>; } )
-std::function<future<>(output_stream<char>&&)> stream_range_as_array(Container val, Func fun) {
-    return [val = std::move(val), fun = std::move(fun)](output_stream<char>&& s) mutable {
-        return do_with(output_stream<char>(std::move(s)), Container(std::move(val)), Func(std::move(fun)), true, [](output_stream<char>& s, const Container& val, const Func& f, bool& first){
-            return s.write("[").then([&val, &s, &first, &f] () {
-                return do_for_each(val, [&s, &first, &f](const typename Container::value_type& v){
-                    auto fut = first ? make_ready_future<>() : s.write(", ");
-                    first = false;
-                    return fut.then([&s, &f, &v]() {
-                        return formatter::write(s, f(v));
-                    });
-                });
-            }).then([&s](){
-                return s.write("]");
-            }).finally([&s] {
-                return s.close();
-            });
-        });
+SEASTAR_CONCEPT(requires requires(Container c, Func aa, output_stream<char> s) {
+    { formatter::write(s, aa(*c.begin())) } -> std::same_as<future<>>;
+})
+std::function<future<>(output_stream<char>&&)> stream_range_as_array(
+  Container val, Func fun) {
+    return [val = std::move(val),
+            fun = std::move(fun)](output_stream<char>&& s) mutable {
+        return do_with(
+          output_stream<char>(std::move(s)),
+          Container(std::move(val)),
+          Func(std::move(fun)),
+          true,
+          [](
+            output_stream<char>& s,
+            const Container& val,
+            const Func& f,
+            bool& first) {
+              return s.write("[")
+                .then([&val, &s, &first, &f]() {
+                    return do_for_each(
+                      val,
+                      [&s, &first, &f](
+                        const typename Container::value_type& v) {
+                          auto fut = first ? make_ready_future<>()
+                                           : s.write(", ");
+                          first = false;
+                          return fut.then([&s, &f, &v]() {
+                              return formatter::write(s, f(v));
+                          });
+                      });
+                })
+                .then([&s]() { return s.write("]"); })
+                .finally([&s] { return s.close(); });
+          });
     };
 }
 
@@ -353,15 +402,17 @@ std::function<future<>(output_stream<char>&&)> stream_range_as_array(Container v
 template<class T>
 std::function<future<>(output_stream<char>&&)> stream_object(T val) {
     return [val = std::move(val)](output_stream<char>&& s) mutable {
-        return do_with(output_stream<char>(std::move(s)), T(std::move(val)), [](output_stream<char>& s, const T& val){
-            return formatter::write(s, val).finally([&s] {
-                return s.close();
-            });
-        });
+        return do_with(
+          output_stream<char>(std::move(s)),
+          T(std::move(val)),
+          [](output_stream<char>& s, const T& val) {
+              return formatter::write(s, val).finally(
+                [&s] { return s.close(); });
+          });
     };
 }
 
 SEASTAR_MODULE_EXPORT_END
-}
+} // namespace json
 
-}
+} // namespace seastar
